903f697e27e066341fd2cde6f393c3a00c08f9a3
Aquavortex <39881794+Aquavortex@users.noreply.github.com>
2022-12-24 18:57:43 +0300

    Add files via upload

---
+++ api/index.php
@@ -0,0 +1,482 @@
+<?php require('onepage.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>        <title>Наручные часы для мужчин</title>
+        
+        <meta name="description" content="Мужские часы, Купить элитные мужские часы, Новые модели, Цены и отзывы">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=480">
+		<link rel="shortcut icon" href="favicon.png" type="image/x-icon">
+        <link rel="stylesheet" type="text/css" href="css/slick.css">    
+		<link rel="stylesheet" type="text/css" href="css/settings.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+<meta property="og:type" content="article"/>
+<meta property="og:title" content="Наручные часы для мужчин"/>
+<meta property="og:description" content="Мужские часы, Купить элитные мужские часы, Новые модели, Цены и отзывы"/>
+
+</head>
+<body>
+	<div class="main-wrap">
+		<div class="section block-1">
+				<div class="sale">скидка<br/><span>%</span></div>
+				<div class="price clearfix">
+                    <div class="old">
+                       Старая цена:<br/>
+                         <span><?= $oldPriceHtml ?> <?= $currencyDisplayHtml ?></span>
+                    </div>
+                    <div class="new">
+                        Новая цена:<br/>
+                        <span><?= $newPriceHtml ?> <?= $currencyDisplayHtml ?></span>
+                    </div>
+                </div>
+              
+			<center style="color: #fff">Акция действует на всей территории Казахстана с <script type="text/javascript">
+    d = new Date(); p = new Date(d.getTime() -  5*86400000);
+    monthA =  '01,02,03,04,05,06,07,08,09,10,11,12'.split(',');
+    var w = p.getDate();
+    document.write(p.getDate() + '.' + monthA[p.getMonth()] +'.'+ p.getFullYear());
+</script>
+ до <script type="text/javascript">
+    d = new Date(); p = new Date(d.getTime() +  1*86400000);
+    monthA =  '01,02,03,04,05,06,07,08,09,10,11,12'.split(',');
+    var w = p.getDate();
+    document.write(p.getDate() + '.' + monthA[p.getMonth()] +'.'+ p.getFullYear());
+</script>
+</center>
+  <a class="button-m scroll" href="#form-z">Оставить заявку</a>
+		</div>
+		<div class="section block-2">
+			<div class="slider">
+				<div><img src="img/slide1.jpg" alt="Часы amst"></div>
+				<div><img src="img/slide2.jpg" alt="Часы amst"></div>
+				<div><img src="img/slide3.jpg" alt="Часы amst"></div>
+				<div><img src="img/slide4.jpg" alt="Часы amst"></div>
+		  	</div>
+		</div>
+		<div class="section block-3">
+			<ul>
+				<li>
+                    <h4>Плавность хода</h4>
+                    Благодаря бельгийскому механизму ход стрелок плавный
+                </li>
+				<li>
+                    <h4>Стиль</h4>
+                    Подойдут как под деловой, так и повседневный стиль
+                </li>
+				<li>
+                    <h4>Удобный ремешок</h4>
+                    Не трескается и не натирает кожу при ношении
+                </li>
+				<li>
+                    <h4>Качественный корпус</h4>
+                    Ударопрочная сталь с IPG покрытием
+                </li>
+				<li>
+                    <h4>Надежное стекло</h4>
+                    Сапфировое с антибликовым покрытием
+                </li>
+				<li>
+                    <h4>Размеры часов</h4>
+                    Диаметр: 46 мм. Толщина: 12 мм. Вес: 130 гр. Длина ремешка: 240 мм.
+                </li>
+			</ul>
+		</div>
+		<div class="section block5">
+			<h2>Доставка и оплата</h2>
+			<ul>
+                <li>
+                    <h5>Оставьте заявку</h5>
+                    Воспользуйтесь формой заказа и оставьте Ваши имя и телефон
+                </li>
+                <li>
+                    <h5>Подтвердите заказ</h5>
+                    Наш менеджер свяжется с Вами и уточнит адрес доставки
+                </li>
+				<li>
+                    <h5>Получите товар</h5>
+                    Заберите посылку и оплатите заказ при получении
+                </li>
+			</ul>
+		</div>
+		<div class="section block6">
+			<h2><b>Отличный подарок</b><br/><span>дорогому человеку или самому себе</span></h2>
+			<img src="img/img-clock.jpg" alt="часы amst">
+			<p><b>AMST - олицетворение статуса и успеха, чувства стиля и вкуса, оригинальности и самодостаточности.</b></p>
+            <p>Возьмите с собой часы AMST на деловую встречу, занятия спортом, на отдых или просто носите их в повседневной жизни, и они будут дарить Вам только приятные эмоции, восхищение со стороны женщин и уважение со стороны мужчин!</p>
+           
+		</div>
+		<div class="section block7">
+			<h2><b>Отзывы покупателей</b></h2>
+			<p>Мы попросили наших покупателей поделиться впечатлениями от заказа</p>
+			<div class="slide-vk">
+				<div><img src="img/vk1.jpg" alt="Отзыв с VK"></div>
+				<div><img src="img/vk2.jpg" alt="Отзыв с VK"></div>
+				<div><img src="img/vk3.jpg" alt="Отзыв с VK"></div>
+			</div>
+		</div>
+		<div class="section block8">
+			<div class="reviews">
+				<img src="img/reviews-img.jpg">
+				<p class="center">«Давно хотел заказать себе часы, выбор пал на эту модель часов. Заказ оформили буквально за 2-3 минуты, доставка в Алматы заняла 7 дней, всё пришло в целости и сохранности, надежно упаковано. Работают отлично, выглядят даже лучше, чем на фото, видно, что материалы качественные, ремешок мягкий и приятный, носить очень комфортно. Недавно попал под дождь, осень же, часы не пострадали от слова совсем! Рекомендую!»</p>
+				<p><b>Айбат, 28 лет, г. Алматы</b></p>
+			</div>
+		</div>
+		<div class="section block9" id="form-z">
+            <h2><b>Часы AMST</b></h2>
+            <p class="sale center">Со скидкой %</p>
+			<div class="container">
+				<div class="price clearfix">
+                    <div class="old">
+                       Старая цена:<br/>
+                        <span><?= $oldPriceHtml ?> <?= $currencyDisplayHtml ?></span>
+                    </div>
+                    <div class="new">
+                        Новая цена:<br/>
+                        <span><?= $newPriceHtml ?> <?= $currencyDisplayHtml ?></span>
+                    </div>
+					
+                </div>
+			</div>
+			<div class="timer">
+			<center style="color: #fff">Акция действует на всей территории Казахстана с <script type="text/javascript">
+    d = new Date(); p = new Date(d.getTime() -  5*86400000);
+    monthA =  '01,02,03,04,05,06,07,08,09,10,11,12'.split(',');
+    var w = p.getDate();
+    document.write(p.getDate() + '.' + monthA[p.getMonth()] +'.'+ p.getFullYear());
+</script>
+ до <script type="text/javascript">
+    d = new Date(); p = new Date(d.getTime() +  1*86400000);
+    monthA =  '01,02,03,04,05,06,07,08,09,10,11,12'.split(',');
+    var w = p.getDate();
+    document.write(p.getDate() + '.' + monthA[p.getMonth()] +'.'+ p.getFullYear());
+</script>
+</center>
+                <!-- <p class="center">До конца акции:</p>
+                <div class="countbox clearfix"></div> -->
+            </div>
+			<div class="main-order-form m1-form" action="" method="post" >
+                <!-- <div><?= countrySelect() ?>
+                    
+                </div>
+                <div><input type="text" name="name" placeholder="Введите Ф.И.О" required=""></div>
+                <div><input type="tel" name="phone" placeholder="Введите телефон" required=""></div> -->
+                <div><button class="button-m openModal" type="button">Оставить заявку</button></div>
+           
+            </div>
+		</div>
+     
+    </div>
+    
+	<center>
+		<center><?= footer() ?></center>
+	</center>
+
+        <script src="js/slick.min.js"></script>        
+		<script src="js/count.js"></script>		
+        <script src="js/scripts.js"></script>	
+        <style>
+        select {
+            position: relative;
+    display: block;
+    outline: 0;
+    border: 0;
+    margin: 0 auto;
+    width: 100%;
+    height: 76px;
+    line-height: 76px;
+    -webkit-border-radius: 35px;
+    -moz-border-radius: 35px;
+    border-radius: 35px;
+    background: #fff;
+    -webkit-box-shadow: inset 0 1px 3px rgba(0,0,0,0.4);
+    -moz-box-shadow: inset 0 1px 3px rgba(0,0,0,0.4);
+    box-shadow: inset 0 1px 3px rgba(0,0,0,0.4);
+    font: 20px 'MuseoSansCyrl-300';
+    text-align: center;
+        }
+        </style>
+
+
+
+
+
+<!-- Modal OrderForm  Start-->
+<div id="openModal" class="modal">
+    <div class="modal-dialog" style="background-color: #3c4029;">
+        <div class="modal-content">
+            <div class="modal-header">
+                <p style="height: 40px;
+                    line-height: 40px;color: #000;font-size: 20px; font-weight: 700;text-align: left;margin: 0;"><!-- PROMOTION: 50% DE RÉDUCTION --></p>
+                <a class="modal__close">
+                <svg class="" viewBox="0 0 24 24"><path d="M19 6.41l-1.41-1.41-5.59 5.59-5.59-5.59-1.41 1.41 5.59 5.59-5.59 5.59 1.41 1.41 5.59-5.59 5.59 5.59 1.41-1.41-5.59-5.59z"/><path d="M0 0h24v24h-24z" fill="none"/></svg>
+            </a>
+            </div>
+            <div class="modal-body">
+                
+                
+                <form class="main-order-form m1-form" action="" method="post" >
+                <div><?= countrySelect() ?>
+                    
+                </div>
+                <div><input type="text" name="name"  placeholder="Введите Ф.И.О" required=""></div>
+                <div><input type="tel" name="phone"  placeholder="Введите телефон" required=""></div>
+                <div><button class="button-m" type="submit">Оставить заявку</button></div>
+           
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+
+.price{
+    direction: ltr;
+display: flex;
+width: 100%;
+text-align: center;
+font-size: 25px;
+font-weight: 700;
+margin-bottom: 15px;
+
+}
+.new_price{
+width: 50%;
+background-color: orange;
+padding: 10px;
+box-sizing: border-box;
+
+}
+.old_price{
+width: 50%;
+background-color: #ccc;
+text-decoration: line-through;
+padding: 10px;
+box-sizing: border-box;
+color: #767676
+}
+.modal *{
+        direction: ltr;
+}
+
+.modal form{
+  width: 100%;padding: 0;
+}
+/* свойства модального окна по умолчанию */
+.modal{
+    position: fixed; /* фиксированное положение */
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: rgba(0,0,0,0.5); /* цвет фона */
+     z-index: 1050;
+    opacity: 0; /* по умолчанию модальное окно прозрачно */
+    -webkit-transition: opacity 200ms ease-out; 
+    -moz-transition: opacity 200ms ease-out;
+    transition: opacity 200ms ease-out; /* анимация перехода */
+    pointer-events: none; /* элемент невидим для событий мыши */
+    margin: 0;
+    padding: 0;
+    font-family: 'Roboto', Arial, sans-serif;
+   visibility: hidden;
+}
+.modal.show{
+  visibility: visible;
+    opacity: 1;
+    width: 100%;
+    pointer-events: auto;
+    overflow-y: auto;
+    cursor: pointer;
+}
+/* ширина модального окна и его отступы от экрана */
+.modal-dialog{
+    max-width: 435px;
+    margin: 30px auto;
+    cursor: auto;
+    transform: scale(0.2);
+    transition: transform 500ms cubic-bezier(0.23, 1, 0.32, 1); /* анимация перехода */
+}
+.animate{
+   transform: scale(1);
+}
+
+/* свойства для блока, содержащего контент модального окна */ 
+.modal-content{
+    position: relative;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -webkit-flex-direction: column;
+    -ms-flex-direction: column;
+    flex-direction: column;
+/*    background-color: #fff;*/
+    -webkit-background-clip: padding-box;
+    background-clip: padding-box;
+/*    border: 1px solid rgba(0,0,0,.2);*/
+    border-radius: .3rem;
+    outline: 0;
+    -webkit-box-shadow: 0 5px 15px rgba(0,0,0,.5);
+    box-shadow: 0 5px 15px rgba(0,0,0,.5);
+}
+
+/*Кнопка Close start*/
+.modal__close{
+    height: 35px;
+    width: 35px;
+    z-index: 1100;
+    cursor: pointer;
+    position: absolute;
+    top: 0;
+    right: 0;
+    margin: 12px;
+    padding: 5px;
+    background: rgba(0,0,0,0.3);
+    border-radius: 50%;
+    -webkit-transition: all 0.5s cubic-bezier(0.23, 1, 0.32, 1);
+    transition: all 0.5s cubic-bezier(0.23, 1, 0.32, 1);box-sizing: border-box;
+}
+.modal__close svg{
+    width: 24px;
+    fill: #fff;
+    pointer-events: none;
+    vertical-align: top;
+}
+.modal__close:hover{
+    background: rgba(0,0,0,0.6);
+}
+/*Кнопка Close end*/
+
+
+/* свойства для заголовка модального окна */
+.modal-header{
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -webkit-align-items: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-box-pack: justify;
+    -webkit-justify-content: space-between;
+    -ms-flex-pack: justify;
+    justify-content: space-between;
+    padding: 15px;
+    border-bottom: 0px solid #eceeef;
+    justify-content: left;
+    height: 60px;
+}
+.modal-title{
+    margin-top: 0;
+    margin-bottom: 0;
+    line-height: 1.5;
+    font-size: 1.25rem;
+    font-weight: 500;
+}
+
+/* свойства для блока, содержащего основное содержимое окна */
+.modal-body{
+    padding: 15px;
+}
+/*.modal form input, .modal form select, .modal form select option, .modal form textarea{
+
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    background: rgb(247, 248, 249);
+    text-align: right;
+    position: relative;
+    display: block;
+    outline: 0px;
+    border: none;
+    border-radius: 3px;
+    width: 100%;
+    box-sizing: border-box;
+    height: 70px;
+    line-height: 70px;
+    margin: 0px auto 15px;
+    padding: 0px 20px 0px 20px;
+    background: rgb(247, 248, 249);
+    text-align: left;
+    font-size: 18px;
+    font-family: "Roboto";
+    font-weight: 400;
+    color: rgb(34, 34, 34);
+    transition: all 0.2s ease 0s;
+    border: 1px solid rgb(180, 180, 180);;
+}*/
+/*    border:2px solid #b2b2b2;
+    text-transform:uppercase;
+    font-size: 16px; 
+    box-sizing: border-box;
+    line-height: 1.55; 
+    padding: 10px;
+    margin-top: 10px;
+    width: 100%;*/
+
+/*.modal-submit-btn{
+    background: #44d759;
+    height: 60px;
+    text-align: center;
+    box-shadow: 0 -5px #178928 inset;
+    color: #fff;
+    font-weight: 700;
+    cursor: pointer;
+    transition: background .4s ease;
+    
+}
+.modal-submit-btn:hover{
+    background:#5EFF75;
+}*/
+
+/*.button-m {
+    width: 100%;
+    height: 80px;
+    line-height: 80px;
+    margin: 0 auto;
+    display: block;
+    background: #fa9535;
+    color: #fff;
+    text-align: center;
+    text-decoration: none;
+    font-size: 30px;
+    border-radius: 3px;
+    outline: none;
+    border: 0;
+    cursor: pointer;
+    font-family: gotham pro,sans-serif;
+}*/
+</style>
+
+<script type="text/javascript">
+$(document).ready(function(){
+    //Open modal
+    $('.openModal').click(function(e) {
+        e.preventDefault();
+        $(".modal").addClass('show');
+        $(".modal-dialog").addClass('animate');
+        
+    });
+    //Close modal
+    $(".modal, .modal__close").click(function(e) {
+        if (e.target == this) { 
+            e.preventDefault();
+            $(".modal").removeClass('show');
+            $(".modal-dialog").removeClass('animate');
+        }
+    })
+});
+</script>
+
+
+
+
+
+</body>
+</html>
